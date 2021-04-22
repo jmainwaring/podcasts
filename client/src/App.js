077d9313490ee7a8cde536d7f0bd5458261bfb70
jmainwaring <jmainwaring@berkeley.edu>
2021-04-22 10:14:04 -0500

    Project setup

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+
+export default () => {
+
+    // Register for API key then make and render the results of this request
+    // podcast = await axios.get('https://api.spotify.com/v1/shows/6UCtBYL29hwhw4YbTdX83N?si=uUGlWCEjS_mXRXEG3l8ADw', {})
+
+
+    // Then, try doing the same thing but by fetching it in my backend then calling it here
+    // podcast = await axios.get('http://localhost:4000/groups', {})
+
+    // After that, keep going through videos and set up docker/kubernetes
+
+    
+    return <div>
+        <h1>Jake's new group podcasting app</h1>
+    </div>
+};
