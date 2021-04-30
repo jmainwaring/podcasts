c00cdad0f7de13bd2a2073bc54e5a0f6992f96cf
jmainwaring <jmainwaring@berkeley.edu>
2021-04-30 18:27:23 -0500

    Added additional services and set up skaffold

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default () => {
-
-    // Register for API key then make and render the results of this request
-    // podcast = await axios.get('https://api.spotify.com/v1/shows/6UCtBYL29hwhw4YbTdX83N?si=uUGlWCEjS_mXRXEG3l8ADw', {})
-
-
-    // Then, try doing the same thing but by fetching it in my backend then calling it here
-    // podcast = await axios.get('http://localhost:4000/groups', {})
-
-    // After that, keep going through videos and set up docker/kubernetes
-
-    
-    return <div>
-        <h1>Jake's new group podcasting app</h1>
+  return (
+    <div className="container">
+      <h1>Create a post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
+  );
 };
